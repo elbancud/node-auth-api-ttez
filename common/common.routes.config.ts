cc147e84c36798a04e48d936ee4955750744d031
elbancud <elbancud@gmail.com>
2022-09-05 08:50:39 +0800

    scaffold

---
+++ common/common.routes.config.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+export class CommonRoutesConfig {
+  // NOTE: Constructor parameters can be defined outside
+  // TODO: Initialize app & name which will be accessed by classes that extends this
+  app: express.Application;
+  name: string;
+  constructor(app: express.Application, name: string) {
+    this.app = app;
+    this.name = name;
+  }
+  getName() {
+    return this.name;
+  }
+}
